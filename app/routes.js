ab8ae30d23c0450f9952a765e8a47ba75aac32dc
SabirLume <sabirlume@gmail.com>
2019-04-21 14:57:27 -0400

    Sorting list && star

--- app/routes.js
+++ app/routes.js
@@ -16,6 +16,7 @@ module.exports = function (app, passport, db) {
     // Recognize text of any language in any format
     //tesseract is grabbing the file from the path the image is being saved
     Tesseract.recognize(req.file.path)
+    //figures what words are in the image
     .then(function (result) {
         console.log(result.text)
         db.collection('documents').save({ user: req.session.passport.user, title: req.body.title, note: result.text }, (err, result) => {
@@ -25,14 +26,14 @@ module.exports = function (app, passport, db) {
         })
       });
     });
-    app.post('/my-notes', (req, res) => {
-      db.collection('documents').save({ user: req.session.passport.user, title: req.body.title, note: req.body.note }, (err, result) => {
-        if (err) return console.log(err)
-        console.log('saved to database')
-        res.redirect('/my-notes')
-      })
+    // app.post('/my-notes', (req, res) => {
+    //   db.collection('documents').save({ user: req.session.passport.user, title: req.body.title, note: req.body.note }, (err, result) => {
+    //     if (err) return console.log(err)
+    //     console.log('saved to database')
+    //     res.redirect('/my-notes')
+    //   })
       
-    })
+    // })
     // show the home page (will also have our login links)
   app.get('/', function (req, res) {
     res.render('index.ejs');
@@ -42,51 +43,59 @@ module.exports = function (app, passport, db) {
     db.collection('documents').find({ user: req.session.passport.user }).toArray((err, result) => {
       if (err) return console.log(err)
       res.render('folders.ejs', {
-        // user : req.user,
-        
         //passing the array of objects into file to use it
         documents: result
       })
-      // console.log("this is showing the results", result)
-      // console.log("this is showing the results" , result)
     })
   });
 
 
   // sent the user to the db  
   //askeed the db for inforamtion and got it back
-  //Twitter example
+  //Twitter example -- when you see links to different websites on twitter they direct you to their server first
+  //then to the link so they can collect data on how many people click the link
+
+  //we are using GET with a insertOne because we can only get an mongoDB id by creating a document . INSERTONE inserts a 
+  // document into a collection. Now we  want to 
+  //get the id from that new document and incorporate it into our my-notes route because we want our notes to have a
+  //unique Id.
+
+
+  //why didn't we just use a post request to create the doc? because we would of already made the doc without 
+  //being able to grab the unique id and putting it into the url
     app.get('/new-note', function (req, res) {
+
     db.collection('documents').insertOne({user: req.session.passport.user,title: null, note: null})
     //this is a promise 
     //then asks for a function what to do when the promise is done  
     .then(function(note){
-      
+
       const objId = note.ops[0]._id
       console.log("get note: ", objId)
-      //whatever db.collection does wait for it then pass it through the function
-      //console.log(note._id);
+      //whatever db.collection does wait for it then pass the information from the document into the function
+      //redirect us to a my-notes page with the unique id
       res.redirect('/my-notes?noteId=' + objId)
       }, () => {})
      
     });
   
-  
+    //this is the route where we go to when we already have a note and want to look it up.
     app.get('/my-notes', function (req, res) {
-      // couldnn't  search mongo by id because they store it as a object . 
+      // couldnn't  search mongo by id because they store it as a object. 
       //find is giving every note that has the same id
+      // not using new becuase new was giving us another new hash different from the one we already have.
       const objId = ObjectId(req.query.noteId)
       console.log("my note: ",objId);
       db.collection('documents').find({ "_id": objId  }).toArray((err, result) => {
-      // res.render('my-notes.ejs');
+        //grabing the [0] index of result because we made objId into an array
       res.render('my-notes.ejs',{result: result[0]})
     })
     });
 
   app.put('/save', (req, res) => {
-      
     console.log("put: ", req.body.qParam)
     db.collection('documents')
+    //finding that qParam we put in our main.js in our DB and updating that DOC in mongo with the information in $set
     .findOneAndUpdate({ "_id": ObjectId(req.body.qParam)  }, {
       $set: {
         note:req.body.note, 
@@ -101,19 +110,18 @@ module.exports = function (app, passport, db) {
     })
   })
 
-
-
-
-  
-    app.delete('/my-notes', (req, res) => {
-      // console.log('Looking for message id', req.body.messageid)
+    app.delete('/folders', (req, res) => {
+      console.log('Looking for message id', req.body.messageid)
       //  converting string into a special object mongo can use id string to id object
       db.collection('documents').findOneAndDelete({ title: req.body.title, note: req.body.note }, (err, result) => {
         if (err) return res.send(500, err)
         res.send('Message deleted!')
         console.log('Message Deleted')
+        console.log(req.body.title, req.body.note)
       })
     })
+
+
 
 
 
--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
-
-var trash = document.getElementsByClassName("fa-trash");
+var trash = document.getElementsByClassName("fas fa-trash");
 var save = document.getElementsByClassName("takeNote");
+var star = document.getElementsByClassName("fa-star");
 
 Array.from(trash).forEach(function(element) {
     element.addEventListener('click', function(){
@@ -10,7 +10,7 @@ Array.from(trash).forEach(function(element) {
       console.log("user: ", user)
       console.log("this is the title:", title)
       console.log("this is the note" , note)
-      fetch('my-notes', {
+      fetch('folders', {
         method: 'delete',
         headers: {
           'Content-Type': 'application/json'
@@ -32,13 +32,13 @@ Array.from(save).forEach(function(element) {
       // const title = this.parentNode.innerText;
       const title= document.getElementById("title").value;
       const note = document.getElementById("text").value;
+      // The URLSearchParams() constructor creates and returns a new URLSearchParams object.
+      // window.location.search returns the query string of the current window everything after the '?'
       const urlParams = new URLSearchParams(window.location.search);
+      console.log('================ urlsearch', urlParams)
+      //this gets everything afer 'noteId' && the '=' in the url allowing us to target just the unique id
       const qParam = urlParams.get('noteId')
-
-      // const noteId = this.parentNode.parentNode.childNodes[3].value
-      // const note = this.parentNode.childNode[2].childNode.innerText;
-      //console.log("this is the noteeeeeeee: ",note)
-      //console.log("this is the titleeeeeeee: ", title)
+      console.log("",qParam)
       fetch('save', {
         method: 'put',
         headers: {
@@ -50,10 +50,62 @@ Array.from(save).forEach(function(element) {
           'note': note,
           'title': title,
           'qParam': qParam
-
         })
       }).then(function (response) {
         // window.location.reload()
       })
     });
   });
+
+  Array.from(star).forEach(function(element) {
+    element.addEventListener('click', function(){
+ 
+    element.classList.toggle("star")
+    fetch('star',{
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'star': star,
+      })
+
+    })
+   
+ })
+
+})
+
+var selected
+
+function dragOver( e ) {
+  if ( isBefore( selected, e.target ) ) e.target.parentNode.insertBefore( selected, e.target )
+  else e.target.parentNode.insertBefore( selected, e.target.nextSibling )
+}
+
+function dragEnd() {
+  selected = null
+}
+
+function dragStart( e ) {
+  //(e) is the short var reference for event object which will be passed to event handlers. Keeps track of events that
+  //occur on the page and allows you to react to them using scripts
+  e.dataTransfer.effectAllowed = "move"
+  // dataTransfer is used to hold the data that is being dragged during a drag && drop operation
+  //effectAllowed provides all the types of operations possible -- none , copy, copyLink, copyMove, move, all , or unititialized
+  e.dataTransfer.setData( "text/plain", null )
+  //dtaTransfer.setData - sets the data for a given type. if data doesnt exist, it is added to the end, so that the last item
+  //item in the types list will be the new format. if data for the type already exists the existing data is replaced inthe same postion
+  selected = e.target
+  //e.target referes to the clicked <li> element
+}
+
+function isBefore( el1, el2 ) {
+  var cur
+  if ( el2.parentNode === el1.parentNode ) {
+    for ( cur = el1.previousSibling; cur; cur = cur.previousSibling ) {
+      //previousSibiling shows the node for the node immdediately preceding specified in its parent's childNode list
+      if (cur === el2) return true
+    }
+  } else return false;
+}
