9671a3822d48da619abbc538bbe66758345c3c0b
SabirLume <sabirlume@gmail.com>
2020-12-24 16:43:48 -0500

    Fixing modules, and dependencies by updating

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var db
 
 
 // configuration ===============================================================
-mongoose.connect(configDB.url, (err, database) => {
+mongoose.connect(configDB.url, { useNewUrlParser: true }, (err, database) => {
   if (err) return console.log(err)
   db = database
   require('./app/routes.js')(app, passport, db);
